a104bf4887df3745493659e32f8a1d9e340c26ff
frankfang <frankfang1990@gmail.com>
2017-07-07 20:44:06 +0800

    run

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+  // 入口 main.js
+  entry: './src/index.js',
+  // 输出
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    //new HtmlwebpackPlugin({
+      //title: 'Webpack-demos',
+      //filename: 'index.html'
+    //}),
+    new OpenBrowserPlugin({
+      url: 'http://localhost:9000'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders:[
+          'style-loader',
+          'css-loader',
+          'autoprefixer-loader',
+          'sass-loader'
+        ] 
+      },
+      {
+        test: /\.less/,
+        loaders:[
+          'style-loader',
+          'css-loader',
+          'autoprefixer-loader',
+          'less-loader'
+        ] 
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        //loader: 'url-loader?limit=8192'
+        loaders: [
+            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack-loader'
+        ]
+      }
+    ]
+  }
+};
